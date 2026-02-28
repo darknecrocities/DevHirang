25a1346bba062f0951bcbb50a7182b29b07eb92b
darknecrocities <parejasarronkian@gmail.com>
2026-03-01 01:08:49 +0800

    fix: fixing and revising system to qualitative

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,7 +5,7 @@ import { AnimatePresence, motion } from 'framer-motion';
 
 const Header = ({ search, setSearch, activeTab, setActiveTab, developers, onSelect }) => {
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-    const tabs = ['Bale', '100 Hirang'];
+    const tabs = ['Contributors', '100 Devs'];
 
     // Filter logic for dropdown
     const searchTerms = search?.toLowerCase().split(' ').filter(Boolean) || [];
